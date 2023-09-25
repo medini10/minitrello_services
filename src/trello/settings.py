0e7acaf1b05caa14772cf230214ad84c242de70c
medini10 <mehdiabedini10@gmail.com>
2023-09-25 17:14:07 +0330

    ststic folder,nginx,production compose added

--- src/trello/settings.py
+++ src/trello/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-ycgy!!p4=ouz-f6$g!scjqvwfg86rfla2b46nq#@p29@wm&p4#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -109,7 +109,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/static/"
+MEDIA_URL = "/static/media/"
+
+STATIC_ROOT = '/vol/web/static'
+MEDIA_ROOT = '/vol/web/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
